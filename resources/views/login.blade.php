ca182532ac104c7ed32708325c8585519d140983
Nisa Nur’aeni_Everest Tech <nisaeni53@gmail.com>
2022-03-31 20:29:36 +0700

    lagi

---
+++ resources/views/login.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.app')
+@section('content')
+
+<body>
+    <h5>Wellcome to Lumos Hotel</h5>
+    <form action="">
+        <div class="container">
+
+        </div>
+    </form>
+</body>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LandingController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,4 @@ Route::get('/', function () {
 });
 
 Route::resource('/landing', LandingController::class);
+Route::resource('/login', LoginController::class);
