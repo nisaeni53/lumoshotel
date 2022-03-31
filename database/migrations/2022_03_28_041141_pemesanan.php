9b6ad8da6fe4d52768c3c5650d6e154af6e536b8
Nisa Nur’aeni_Everest Tech <nisaeni53@gmail.com>
2022-03-31 08:07:29 +0700

    awal

---
+++ database/migrations/2022_03_28_041141_pemesanan.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('pemesanan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_kamar');
+            $table->foreignId('id_login');
+            $table->string('nama_tamu');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
---
+++ resources/views/landing.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.app')
+@section('content')
+    <link rel="stylesheet" href="{{asset ('css/')}}/landing.css">
+    <div class="container">
+        <div class="jumbroton">
+            {{-- <div class="photo-jumbroton mt-2 d-flex">
+                <img src="image/imgutama.png" alt="" style="width: 100%">
+            </div> --}}
+            <div class="judul">
+                <h1>Enjoy Your Dream Vacation</h1>
+            </div>
+        </div>
+    </div>
+    
+@endsection
